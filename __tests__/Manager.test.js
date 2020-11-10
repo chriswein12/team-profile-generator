7e42f3d1016d7b9f990683e87c9f6361996bc1fe
Chris Wein <chriswein12@yahoo.com>
2020-11-09 22:54:19 -0600

    added class and test for Manager

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,1 +1,14 @@
+const Employee = require('../lib/Employee');
 const Manager = require('../lib/Manager');
+
+test('creates a Manager object with an office telephone number', () => {
+    const manager = new Manager(1, 'Chris', 'test@gmail.com', 100);
+
+    expect(manager.officeNumber).toEqual(100);
+});
+
+test('verifies role', () => {
+    const manager = new Manager(1, 'Chris', 'test@gmail.com', 100);
+
+    expect(manager.getRole()).toBe('Manager');
+});
