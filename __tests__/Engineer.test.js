ac9757fe8a82724e78ae4b3caf175985c5bfc929
Chris Wein <chriswein12@yahoo.com>
2020-11-09 22:33:17 -0600

    Employee and Engineer classes and tests complete

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -3,19 +3,19 @@ const Employee = require('../lib/Employee');
 const Engineer = require('../lib/Engineer');
 
 test('creates an Engineer object with a github username', () => {
-    const engineer = new Engineer('chriswein12');
+    const engineer = new Engineer(1, 'Chris', 'test@gmail.com', 'chriswein12');
 
     expect(engineer.github).toBe('chriswein12');
 });
 
 test('gets GitHub username', () => {
-    const engineer = new Engineer('chriswein12');
+    const engineer = new Engineer(1, 'Chris', 'test@gmail.com', 'chriswein12');
 
     expect(engineer.github).toBe('chriswein12')
 })
 
 test('verifies role', () => {
-    const engineer = new Engineer('chriswein12');
+    const engineer = new Engineer(1, 'Chris', 'test@gmail.com', 'chriswein12');
 
     expect(engineer.getRole()).toBe('Engineer');
 });
